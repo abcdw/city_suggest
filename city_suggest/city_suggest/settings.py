c6327768ea049df55814324fc56c248f242ac149
abcdw <andrewtropin@gmail.com>
2014-06-28 13:48:34 +0400

    Add cities_light and suggest applications to project

--- city_suggest/city_suggest/settings.py
+++ city_suggest/city_suggest/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cities_light',
+    'suggest',
 )
 
 MIDDLEWARE_CLASSES = (
